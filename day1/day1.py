a4f8feb60424492ab4ac609ca54b84ad8a0f4bdb
JorgeMJM <jorgejimenezmontesdeoca@gmail.com>
2022-06-01 15:47:54 -0500

    Day 1 Update

--- day1/day1.py
+++ day1/day1.py
@@ -26,3 +26,35 @@ def exercise2():
 def exercise3():
     name = input("What's your name?")
     print(len(name))
+
+#Instructions
+#Write a program that switches the values stored in the variables a and b.
+
+#Warning. Do not change the code on lines 1-4 and 12-18. Your program should work for different inputs. e.g. any value of a and b.
+
+#Example input
+# a:3
+# b:5
+
+#Example Output
+# a:5
+# b:3
+
+def exercise4():
+    a = int(input("a: "))
+    b = int(input("b: "))
+
+    a = a + b
+    b = a - b
+    a = a - b
+
+    print("")
+    print(f"a: {a}")
+    print(f"b: {b}")
+
+# other way to do it is:
+# aux1 = a
+# aux2 = b
+# a = aux2
+# b = aux1
+#This will work for the codingroom, but it is an inneficcient way to do it because it will take more mememory
