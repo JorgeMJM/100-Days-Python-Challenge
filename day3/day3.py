9f3cb1378023308215307233e26342adfa04f51f
JorgeMJM <jorgejimenezmontesdeoca@gmail.com>
2022-06-01 16:16:33 -0500

    Udapted Day 3 and Main

--- day3/day3.py
+++ day3/day3.py
@@ -48,3 +48,68 @@ def leapYear():
             print("Leap year.")
     else:
         print("Not leap year.")
+
+#Congratulations, you've got a job at Python Pizza. Your first job is to build an automatic pizza order program.
+#Based on a user's order, work out their final bill.
+#Small Pizza: $15
+#Medium Pizza: $20
+#Large Pizza: $25
+#Pepperoni for Small Pizza: +$2
+#Pepperoni for Medium or Large Pizza: +$3
+#Extra cheese for any size pizza: + $1
+
+def pizzaChanllenge():
+    print("Welcome to Python Pizza Deliveries!")
+    size = input("What size pizza do you want? S, M, or L ")
+    add_pepperoni = input("Do you want pepperoni? Y or N ")
+    extra_cheese = input("Do you want extra cheese? Y or N ")
+
+    if size == "S":
+        total = 15
+        if add_pepperoni == "Y":
+            total += 2
+            if extra_cheese == "Y":
+                total += 1
+                print(f"Your final bill is: ${total}")
+            else:
+                print(f"Your final bill is: ${total}")
+        else:
+            if extra_cheese == "Y":
+                total += 1
+                print(f"Your final bill is: ${total}")
+            else:
+                print(f"Your final bill is: ${total}")
+    elif size == "M":
+        total = 20
+        if add_pepperoni == "Y":
+            total += 3
+            if extra_cheese == "Y":
+                total += 1
+                print(f"Your final bill is: ${total}")
+            else:
+                print(f"Your final bill is: ${total}")
+        else:
+            if extra_cheese == "Y":
+                total += 1
+                print(f"Your final bill is: ${total}")
+            else:
+                print(f"Your final bill is: ${total}")
+    elif size == "L":
+        total = 25
+        if add_pepperoni == "Y":
+            total += 3
+            if extra_cheese == "Y":
+                total += 1
+                print(f"Your final bill is: ${total}")
+            else:
+                print(f"Your final bill is: ${total}")
+        else:
+            if extra_cheese == "Y":
+                total += 1
+                print(f"Your final bill is: ${total}")
+            else:
+                print(f"Your final bill is: ${total}")
+    else:
+        print("Select a pizza size please")
+
+
