7c65da65d683e523ed6240c2b56a22cd73cf4d8f
JorgeMJM <jorgejimenezmontesdeoca@gmail.com>
2022-06-01 12:09:34 -0500

    Day 1 Update

--- day1/day1.py
+++ day1/day1.py
@@ -8,3 +8,18 @@ def exercise1():
     print("Day 1 - Python Print Function")
     print("The function is declared like this:")
     print("print('what to print)")
+
+# On exercise2, we are asked to print the next text:
+#Day 1 - String Manipulation
+#String Concatenation is done with the "+" sign.
+#e.g. print("Hello " + "world")
+#New lines can be created with a backslash and n.
+#So we are going to inserted in a function to print
+
+def exercise2():
+    print("Day 1 - String Manipulation")
+    print('String Concatenation is done with the "+" sign.')
+    print('e.g. print("Hello " + "world")')
+    print("New lines can be created with a backslash and n.")
+
+def exercise3
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from day1 import day1
 
 day1.exercise1()
-
+print("")
+day1.exercise2()
