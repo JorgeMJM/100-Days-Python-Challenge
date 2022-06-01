b56eac02763e6125fe03e90cc2cf0a9fb5479cb3
JorgeMJM <jorgejimenezmontesdeoca@gmail.com>
2022-06-01 11:56:17 -0500

    Day 1 Update

---
+++ day1/day1.py
@@ -0,0 +1,10 @@
+#We need to print the next output
+
+#Day 1 - Python Print Function
+#The function is declared like this:
+#print('what to print')
+
+def printText():
+    print("Day 1 - Python Print Function")
+    print("The function is declared like this:")
+    print("print('what to print)")
