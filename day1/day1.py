757c16edd173d51b21f8278d3b82c47a3ee26af2
JorgeMJM <jorgejimenezmontesdeoca@gmail.com>
2022-06-01 11:59:34 -0500

    Day 1 Update

--- day1/day1.py
+++ day1/day1.py
@@ -4,7 +4,7 @@
 #The function is declared like this:
 #print('what to print')
 
-def excerice1():
+def exercise1():
     print("Day 1 - Python Print Function")
     print("The function is declared like this:")
     print("print('what to print)")
